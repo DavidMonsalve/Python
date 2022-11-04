4a92e8220c3288052aaf49fbb6386ef106173870
David Monsalve <davidmonsalve717@gmail.com>
2022-11-04 15:05:22 -0300

    clima

---
+++ clima de cualquier ciudad/__main__.py
@@ -0,0 +1,19 @@
+import requests
+
+API_KEY = "dc3988f620cb98abc864229be47306bc"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data["weather"][0]["description"]
+    print(weather)
+    temperature = data["main"]["temp"] - 273.15
+    print(str(round(temperature)) + " C")
+else:
+    print("An error occurred. ")
+
+input("Press enter to end. ")
